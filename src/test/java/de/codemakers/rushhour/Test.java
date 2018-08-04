940bcf0568baf89fc46516f103bb76c9c9878183
Paul Hagedorn <panzer1119@gmail.com>
2018-08-04 15:39:41 +0200

    Fixed some things
    Signed-off-by: Paul Hagedorn <panzer1119@gmail.com>

--- src/test/java/de/codemakers/rushhour/Test.java
+++ src/test/java/de/codemakers/rushhour/Test.java
@@ -7,6 +7,7 @@ import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 import java.util.Arrays;
 import java.util.Map;
+import java.util.Random;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class Test {
@@ -26,6 +27,21 @@ public class Test {
         for (Vehicle vehicle : Vehicle.values()) {
             COLORS.put(vehicle.getId(), vehicle.getColor());
         }
+        final Random random = new Random();
+        for (Vehicle vehicle : Vehicle.values()) {
+            if (random.nextBoolean()) {
+                final int x_1 = random.nextInt(BOARD.length - 1);
+                final int y_1 = random.nextInt(BOARD[0].length - 1);
+                int x_2 = x_1;
+                int y_2 = y_1;
+                if (random.nextBoolean()) { //vertical
+                    y_2 = y_1 + ((vehicle.isCar() ? 1 : 2) * (y_1 < BOARD[0].length ? 1 : -1));
+                } else { // horizontal
+                    x_2 = x_1 + ((vehicle.isCar() ? 1 : 2) * (x_1 < BOARD.length ? 1 : -1));
+                }
+                putVehicle(vehicle, x_1, y_1, x_2, y_2);
+            }
+        }
     }
     
     public static final void main(String[] args) {
@@ -37,17 +53,19 @@ public class Test {
         FRAME.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
         FRAME.setLayout(new BorderLayout());
         FRAME.setPreferredSize(new Dimension(600, 600));
+        PANEL_BOARD.setLayout(new GridLayout(6, 6));
         for (int x = 0; x < BUTTONS_BOARD.length; x++) {
             for (int y = 0; y < BUTTONS_BOARD[x].length; y++) {
                 BUTTONS_BOARD[x][y] = new JButton();
+                PANEL_BOARD.add(BUTTONS_BOARD[x][y]);
             }
         }
         initListeners();
-        PANEL_BOARD.setLayout(new GridLayout(6, 6));
         FRAME.add(PANEL_BOARD, BorderLayout.CENTER);
         FRAME.pack();
         FRAME.setLocationRelativeTo(null);
         FRAME.setVisible(true);
+        updateBoard();
     }
     
     public static final void initListeners() {
@@ -105,6 +123,25 @@ public class Test {
     
     public static final void action(ActionEvent event, JButton button, int x, int y) {
         System.out.println("Clicked: " + x + "|" + y);
+    }
+    
+    public static final void putVehicle(Vehicle vehicle, int x_1, int y_1, int x_2, int y_2) {
+        if (!checkBounds(x_1, y_1, x_2, y_2)) {
+            throw new IllegalArgumentException("Wrong coordinates");
+        }
+        final int x_min = Math.min(x_1, x_2);
+        final int x_max = Math.max(x_1, x_2);
+        final int y_min = Math.min(y_1, y_2);
+        final int y_max = Math.max(y_1, y_2);
+        for (int x = x_min; x <= x_max; x++) {
+            for (int y = y_min; y <= y_max; y++) {
+                BOARD[x][y] = vehicle.getId();
+            }
+        }
+    }
+    
+    public static final boolean checkBounds(int x_1, int y_1, int x_2, int y_2) {
+        return !(Math.min(x_1, x_2) < 0 || Math.min(y_1, y_2) < 0 || Math.max(x_1, x_2) >= BOARD.length || Math.max(y_1, y_2) >= BOARD[0].length);
     }
     
 }
