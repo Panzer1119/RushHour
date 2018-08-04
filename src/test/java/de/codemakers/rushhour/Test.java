1053cb569e70463ea3161b8321a01fa0304ebbad
Paul Hagedorn <panzer1119@gmail.com>
2018-08-04 16:14:45 +0200

    Added another test mode
    Signed-off-by: Paul Hagedorn <panzer1119@gmail.com>

--- src/test/java/de/codemakers/rushhour/Test.java
+++ src/test/java/de/codemakers/rushhour/Test.java
@@ -27,21 +27,38 @@ public class Test {
         for (Vehicle vehicle : Vehicle.values()) {
             COLORS.put(vehicle.getId(), vehicle.getColor());
         }
+        int x = 0;
+        int y = 0;
+        final boolean test = true;
         final Random random = new Random();
         for (Vehicle vehicle : Vehicle.values()) {
-            if (random.nextBoolean()) {
-                final int x_1 = random.nextInt(BOARD.length - 1);
-                final int y_1 = random.nextInt(BOARD[0].length - 1);
-                int x_2 = x_1;
-                int y_2 = y_1;
-                if (random.nextBoolean()) { //vertical
-                    y_2 = y_1 + ((vehicle.isCar() ? 1 : 2) * (y_1 < BOARD[0].length ? 1 : -1));
-                } else { // horizontal
-                    x_2 = x_1 + ((vehicle.isCar() ? 1 : 2) * (x_1 < BOARD.length ? 1 : -1));
+            if (vehicle == Vehicle.EMPTY) {
+                continue;
+            }
+            if (test) {
+                if (random.nextBoolean()) {
+                    final int x_1 = random.nextInt(BOARD.length - 1);
+                    final int y_1 = random.nextInt(BOARD[0].length - 1);
+                    int x_2 = x_1;
+                    int y_2 = y_1;
+                    if (random.nextBoolean()) { //vertical
+                        y_2 = y_1 + ((vehicle.isCar() ? 1 : 2) * (y_1 < (BOARD[0].length - (vehicle.isCar() ? 1 : 2)) ? 1 : -1));
+                    } else { // horizontal
+                        x_2 = x_1 + ((vehicle.isCar() ? 1 : 2) * (x_1 < (BOARD.length - (vehicle.isCar() ? 1 : 2)) ? 1 : -1));
+                    }
+                    putVehicle(vehicle, x_1, y_1, x_2, y_2);
                 }
-                putVehicle(vehicle, x_1, y_1, x_2, y_2);
+            } else {
+                if (x >= BOARD.length) {
+                    x = 0;
+                    y ++;
+                }
+                System.out.println(vehicle);
+                putVehicle(vehicle, x, y, x + (vehicle.isCar() ? 1 : 2), y);
+                x += (vehicle.isCar() ? 2 : 3);
             }
         }
+        System.out.println("Finished static init");
     }
     
     public static final void main(String[] args) {
@@ -54,8 +71,8 @@ public class Test {
         FRAME.setLayout(new BorderLayout());
         FRAME.setPreferredSize(new Dimension(600, 600));
         PANEL_BOARD.setLayout(new GridLayout(6, 6));
-        for (int x = 0; x < BUTTONS_BOARD.length; x++) {
-            for (int y = 0; y < BUTTONS_BOARD[x].length; y++) {
+        for (int y = 0; y < BUTTONS_BOARD.length; y++) {
+            for (int x = 0; x < BUTTONS_BOARD[y].length; x++) {
                 BUTTONS_BOARD[x][y] = new JButton();
                 PANEL_BOARD.add(BUTTONS_BOARD[x][y]);
             }
@@ -65,6 +82,7 @@ public class Test {
         FRAME.pack();
         FRAME.setLocationRelativeTo(null);
         FRAME.setVisible(true);
+        System.out.println("BOARD: " + Arrays.deepToString(BOARD));
         updateBoard();
     }
     
@@ -116,9 +134,15 @@ public class Test {
     public static final void updateBoard() {
         for (int x = 0; x < BOARD.length; x++) {
             for (int y = 0; y < BOARD[x].length; y++) {
+                //System.out.println("COLOR: " + COLORS.get(BOARD[x][y]));
                 BUTTONS_BOARD[x][y].setBackground(COLORS.get(BOARD[x][y]));
+                BUTTONS_BOARD[x][y].setOpaque(true);
+                BUTTONS_BOARD[x][y].setForeground(COLORS.get(BOARD[x][y]));
+                BUTTONS_BOARD[x][y].setText(String.format("%s;X:%d;Y:%d", Vehicle.ofId(BOARD[x][y]).name(), x, y));
             }
         }
+        PANEL_BOARD.revalidate();
+        PANEL_BOARD.repaint();
     }
     
     public static final void action(ActionEvent event, JButton button, int x, int y) {
@@ -133,6 +157,14 @@ public class Test {
         final int x_max = Math.max(x_1, x_2);
         final int y_min = Math.min(y_1, y_2);
         final int y_max = Math.max(y_1, y_2);
+        for (int x = x_min; x <= x_max; x++) {
+            for (int y = y_min; y <= y_max; y++) {
+                if (BOARD[x][y] != Vehicle.EMPTY.getId()) {
+                    System.err.println(String.format("(%d:%d)-(%d:%d) is already occupied", x_min, y_min, x_max, y_max));
+                    return;
+                }
+            }
+        }
         for (int x = x_min; x <= x_max; x++) {
             for (int y = y_min; y <= y_max; y++) {
                 BOARD[x][y] = vehicle.getId();
